14b3e95efbb4d4f13fe21dd05bfeedf4a4efa736
dbirman <danbirman@gmail.com>
2015-11-03 12:35:45 -0800

    Updated + removed extra dat

--- static/js/task.js
+++ static/js/task.js
@@ -39,13 +39,13 @@ function cohcon() {
 		phases.dirKnown = [12];
 		phases.post3 = 13;
 	} else if (taskDir==2) {
-		phases.catTask = [3,5,7,9,11];
-		phases.post1 = 12;
-		phases.teachRule = 13;
-		phases.post2 = 14;
-		phases.ending = 15;
-		phases.catKnown = [16,18];
-		phases.post3 = 19;
+		phases.catTask = [3,5,7,9,11,13,15,17,19,21];
+		phases.post1 = 22;
+		phases.teachRule = 23;
+		phases.post2 = 24;
+		phases.ending = 25;
+		phases.catKnown = [26,28];
+		phases.post3 = 29;
 	}
 
 	var segs = {};
@@ -167,10 +167,11 @@ function cohcon() {
 		}
 	} else if (taskDir==2) {
 
-		var readylen = [1.25, 0, 0,0,0];
-		var resptime = [2.5,2.25,1.75,1.75,1.75];
-		var resplen = [1.5,1,1,1,1];
-		var getred = [1,0,0,0,0];
+		var readylen = [1.25, 0, 0,0,0,0,0,0,0,0,0,0];
+		var resptime = [2.5,2.25,1.75,1.5,1.5,1.25,1.25,1.25,1.25,1.2,1.2,1.2];
+		var resplen = [1.5,1,.75,.75,.75,.75,.6,.6,.6,.6,.6,.55,.55];
+		var getred = [1,0,0,0,0,0,0,0,0,0,0,0,0];
+
 
 		for (var i = 0; i < phases.catTask.length; i++) {
 			ct = phases.catTask[i];
@@ -180,14 +181,14 @@ function cohcon() {
 			task[0][ct].seglen = [readylen[i], .650, .650, 1.000, .65,resptime[i],resplen[i]];
 			task[0][ct].numTrials = params.trials;
 			task[0][ct].parameter = {};
-			task[0][ct].parameter.match = [0,1];
+			task[0][ct].parameter.match = [0,1,1,0];
 			task[0][ct].parameter.categories = 1;
 			task[0][ct].parameter.known = 0;
 			task[0][ct].parameter.showresp = 1;
 			task[0][ct].parameter.getready = getred[i];
 			task[0][ct].parameter.block = i;
 			if (task[0][ct].parameter.categories==1) {
-				task[0][ct].parameter.direction = [0,1];
+				task[0][ct].parameter.direction = [0,1,0,1];
 				task[0][ct].parameter.nomatchdir = [0];
 			} else {
 				task[0][ct].parameter.direction = params.directions;
@@ -222,17 +223,17 @@ function cohcon() {
 
 			task[0][ct] = {};
 			task[0][ct].waitForBacktick = 0;
-			task[0][ct].seglen = [0, .650, .650, 1.000, .667,1.75,1];
+			task[0][ct].seglen = [0, .650, .650, 1.000, .65,1.2,.55];
 			task[0][ct].numTrials = params.trials;
 			task[0][ct].parameter = {};
-			task[0][ct].parameter.match = [0,1];
+			task[0][ct].parameter.match = [0,1,1,0];
 			task[0][ct].parameter.categories = 1;
 			task[0][ct].parameter.known = 1;
 			task[0][ct].parameter.showresp = 0;
 			task[0][ct].parameter.getready = 0;
 			task[0][ct].parameter.block = i;
 			if (task[0][ct].parameter.categories==1) {
-				task[0][ct].parameter.direction = [0,1];
+				task[0][ct].parameter.direction = [0,1,0,1];
 				task[0][ct].parameter.nomatchdir = [0];
 			} else {
 				task[0][ct].parameter.direction = params.directions;
@@ -272,8 +273,22 @@ function cohcon() {
 	jglData.known = [];
 	jglData.trial = [];
 	jglData.block = [];
+	jglData.corrLimit = 1;
+	jglData.dirC = zeros(64);
+	jglData.dirN = zeros(64);
+	stimulus.lastFrame = jglGetSecs();
+
 
 	startPhase(task[0]);
+}
+
+var rot2ind = function(a,b) {
+	return a*8+b;
+}
+var ind2rot = function(ind) {
+	var a = floor(ind/8);
+	var b = ind % 8;
+	return [a,b];
 }
 
 var startBlockCallback = function(task, myscreen) {
@@ -284,16 +299,14 @@ var startBlockCallback = function(task, myscreen) {
 
 var endTrialCallbackPrac = function(task,myscreen) {
 	jglData.correct[jglData.correct.length-1] = stimulus.gotResp;
+	var ind = rot2ind(stimulus.num1,stimulus.num2);
+	if (stimulus.gotResp==1) {jglData.dirC[ind] += 1;}
 	return [task,myscreen];
 }
 
 var startTrialCallback = function(task, myscreen) {
-
 	stimulus.categories = task.thistrial.categories;
 
-	if(task.thistrial.crit) {
-		task.thistrial.seglen[stimulus.seg.resp] = 5;
-	}
 	jglData.responses.push(-1);
 	jglData.correct.push(0);
 	jglData.direction.push(task.thistrial.direction);
@@ -304,24 +317,49 @@ var startTrialCallback = function(task, myscreen) {
 	jglData.block.push(task.thistrial.block);
 	stimulus.gotResp = 0;
 
-	var flip = [1, 0];
-	if (stimulus.categories) {
-		stimulus.rot1 = randomElement(stimulus.categoryGroups[task.thistrial.direction]) * Math.PI * 2 / 8;
-		if (task.thistrial.match) {
-			// matching trial, use same category
-			stimulus.rot2 = randomElement(stimulus.categoryGroups[task.thistrial.direction]) * Math.PI * 2 / 8;
-		} else {
+	if (!any(lessThan(jglData.dirC,jglData.corrLimit))) {jglData.corrLimit += 1;}
 
-			stimulus.rot2 = randomElement(stimulus.categoryGroups[flip[task.thistrial.direction]]) * Math.PI * 2 / 8;
-		}
-	} else {
-		stimulus.rot1 = task.thistrial.direction * Math.PI * 2 / 8;
-		if (task.thistrial.match) {
-			stimulus.rot2 = stimulus.rot1;
+	var repick = true;
+		
+	var retry = 0;
+	while (repick) {		
+		var flip = [1, 0];
+		if (stimulus.categories==1) {
+			// pick two directions
+			stimulus.num1 = randomElement(stimulus.categoryGroups[task.thistrial.direction]);
+			if (task.thistrial.match==1) {
+				// matching trial, use same category
+				stimulus.num2 = randomElement(stimulus.categoryGroups[task.thistrial.direction]);
+			} else {
+				stimulus.num2 = randomElement(stimulus.categoryGroups[flip[task.thistrial.direction]]);
+			}
+			// we picked two numbers, check to see if they are in the available indexes we haven't succeded at
+			failInds = find(lessThan(jglData.dirC,jglData.corrLimit));
+			var curInd = rot2ind(stimulus.num1,stimulus.num2);
+			for (var i in failInds) {
+				val = failInds[i];
+				if (curInd==val) {
+					repick = false; // we found our index in the avail, no repick needed
+					break
+				}
+			}
 		} else {
-			stimulus.rot2 = task.thistrial.direction * Math.PI * 2 / 8 + task.thistrial.nomatchdir;
+			stimulus.num1 = task.thistrial.direction;
+			if (task.thistrial.match==1) {
+				stimulus.num2 = stimulus.num1;
+			} else {
+				stimulus.num2 = task.thistrial.direction + task.thistrial.nomatchdir;
+			}
+			repick = false;
 		}
+		retry += 1;
+		if (retry > 80) {repick = false;}
 	}
+	var curInd = rot2ind(stimulus.num1,stimulus.num2);
+	jglData.dirN[curInd] += 1;
+
+	stimulus.rot1 = stimulus.num1 * Math.PI * 2 / 8;
+	stimulus.rot2 = stimulus.num2 * Math.PI * 2 / 8;
 	jglData.rot1.push(stimulus.rot1);
 	jglData.rot2.push(stimulus.rot2);
 
@@ -333,11 +371,13 @@ var startTrialCallback = function(task, myscreen) {
 	stimulus.dots.x = multiply(stimulus.dots.R,cos(stimulus.dots.T));
 	stimulus.dots.y = multiply(stimulus.dots.R,sin(stimulus.dots.T));
 
+	stimulus.lastFrame = jglGetSecs();
+
 
 	// contrast
 	// convert to hex color
 
-  return [task, myscreen];
+  	return [task, myscreen];
 }
 
 var getResponseCallback = function(task, myscreen) {
@@ -379,6 +419,9 @@ var startSegmentCallback = function(task, myscreen) {
 }
 
 var screenUpdateCallback = function(task, myscreen) {
+	var now = jglGetSecs();
+	var elapsed = now - stimulus.lastFrame;
+	stimulus.lastFrame = now;
 	jglClearScreen(0);
 
 	var segs = stimulus.seg;
@@ -391,13 +434,13 @@ var screenUpdateCallback = function(task, myscreen) {
 			upFix('#ffffff');
 			break;
 		case segs.stimulus:
-			upDots(task);
+			upDots(task,elapsed);
 			break;
 		case segs.delay:
 			upFix('#ffffff');
 			break;
 		case segs.test:
-			upDots(task);
+			upDots(task,elapsed);
 			break;
 		case segs.resp:
 			if (task.thistrial.showresp && task.thistrial.block < 2) {
@@ -454,22 +497,22 @@ function upFix(color) {
 	jglFixationCross(1,0.1,color,[0,0]);
 }
 
-function upDots(task) {
-	stimulus.dots = updateDots(task,stimulus.dots);
+function upDots(task,elapsed) {
+	stimulus.dots = updateDots(task,stimulus.dots,elapsed);
 
 	jglPoints2(stimulus.dots.x, stimulus.dots.y, 0.2, '#ffffff');
 }
 
-function updateDots(task,dots) {
+function updateDots(task,dots,elapsed) {
 
 	// Check frequency? Not sure how to do this...
-	freq_factor = 0.24;
+	freq_factor = 12*elapsed;
 
 	// dots.x = add(dots.y,);
 
 	// Flip dots back if they go too far
 	for (var i=0;i<dots.R.length;i++) {
-		if (dots.R[i] > dots.maxR) {
+		if (dots.R[i] > dots.maxR && dots.holdx[i] > 0) {
 			dots.holdx[i] = -dots.holdx[i];
 		}
 	}
