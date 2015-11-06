8f80ac874a35683d117a603bb311a0013ff68d45
dbirman <danbirman@gmail.com>
2015-11-06 12:10:16 -0800

    Moved to conceptual figure

--- analysis/matlab_behavior_MTurk_getLongSurvey.m
+++ analysis/matlab_behavior_MTurk_getLongSurvey.m
@@ -14,134 +14,136 @@ global analysis
 analysis.dir = '~/proj/freedman_rep/';
 
 %%
-append = 'dir';
-show = 0;
-%% Run
+appends = {'dir','cat','long'};
 
-names = {};
-
-files = dir(sprintf('~/proj/freedman_rep/%smat/*.mat',append));
-
-params = [];
-signals = {[],[],[],[]};
-pcorrects = {[],[],[],[]};
-pullfroms = {1:25,26:50,51:75,75:100};
-pcorrectstes = [];
-ruleDir = {};
-ruleCat = {};
-for fi = 1:length(files)
-    load(fullfile(sprintf('~/proj/freedman_rep/%smat',append),files(fi).name));
+for ai = 1:length(appends)
+    append = appends{ai};
+    %% data we're going to collect
     
-    if length(fields(jglData.postSurvey))>1
-        csvf = fullfile(analysis.dir,sprintf('%scsv',append));
-        if ~isdir(csvf), mkdir(csvf); end
-
-
-        wid_i = strfind(files(fi).name,':');
-        wid = files(fi).name(1:wid_i-1);
-        csvf = fullfile(csvf,sprintf('data_%s.csv',wid));
-
-        names{end+1} = wid;
-
-        direct = strfind(append,'dir');
-        if show
-            disp('****************************');
-            disp(sprintf('File: %s',files(fi).name));
-            disp('****************************');
-            if strcmp(append,'cat')
-                disp(sprintf('WID: %i',fi));
-            else
-                disp(sprintf('WID: %i',47+fi));
-            end
-            %% print survey responses
-
-            if isfield(jglData.postSurvey,'ruleknownDir')
-                direct = 1;
-                disp(sprintf('Knowledge of Dir Rule: %s',jglData.postSurvey.ruleDir));
-
-                disp(sprintf('Post Explanation, did they know?: %s',jglData.postSurvey.ruleknownDir));
-                %         disp(sprintf('Knowledge of Cat Rule: %s',jglData.postSurvey.ruleCat));
-                %         disp(sprintf('Post Explanation, did they know?: %s',jglData.postSurvey.ruleknownCat));
-            else
-                disp(sprintf('Knowledge of Cat Rule: %s',jglData.postSurvey.ruleCat));
-                disp(sprintf('Post Explanation, did they know?: %s',jglData.postSurvey.ruleknownCat));
-            end
-            disp(sprintf('Comments: %s',jglData.postSurvey.comments));
-            disp(sprintf('Were they attending? (claimed): %s',jglData.postSurvey.attention));
-            disp(sprintf('Were they fixating? (claimed): %s',jglData.postSurvey.fixation));
-            disp(sprintf('Eye jitter / tracking? %s',jglData.postSurvey.motion));
-            disp(sprintf('Screen Problems): %i',jglData.postSurvey.smoothness));
-        end
-
-        if direct > 0 
-
-            for pi = 1:length(pullfroms)
-                pullfrom = pullfroms{pi};
-                sig = signals{pi};
-                pcorr = pcorrects{pi};
-                [signals{pi}, pcorrects{pi}] = pullAndBin(pullfrom,jglData,sig,pcorr);
-            end
-
-                % add 'diff' field
+    dataOut = struct;
+    demoFields = {'age','gender'};
+    psFields = {'ruleDir','ruleKnownDir','ruleCat','ruleKnownCat','smoothness','attention','fixation','motion','before','comments'};
+    
+    for di = 1:length(demoFields)
+        cd = demoFields{di};
+        comm = sprintf('%s = {};',cd);
+        eval(comm);
+        aname = sprintf('a%s',cd);
+        if ~exist(aname), comm = sprintf('%s = {};',aname); eval(comm); end
+    end
+    for di = 1:length(psFields)
+        cd = psFields{di};
+        comm = sprintf('%s = {};',cd);
+        eval(comm);
+        aname = sprintf('a%s',cd);
+        if ~exist(aname), comm = sprintf('%s = {};',aname); eval(comm); end
+    end
+    %% Run
+    
+    names = {};
+    
+    files = dir(sprintf('~/proj/freedman_rep/data/%smat/*.mat',append));
+    
+    for fi = 1:length(files)
+        load(fullfile(sprintf('~/proj/freedman_rep/data/%smat',append),files(fi).name));
+        
+        for di = 1:length(demoFields)
+            cd = demoFields{di};
             
-            %             pcorrectstes = [pcorrectstes;fit.pcorrectste];
-
+            if isfield(jglData.demographics,cd)
+                comm = sprintf('%s{end+1} = jglData.demographics.%s',cd,cd);
+                eval(comm);
+            end
         end
-
-        %% convert everything to CSV - output
-
-
-%         fieldz = {'responses','correct','direction','categories','match','rot1','rot2','known','trial','block'};
-% 
-%         data = zeros(length(jglData.responses),length(fieldz));
-%         for i = 1:length(fieldz)
-%             field = fieldz{i};
-% 
-%             data(:,i) = jglData.(fieldz{i});
-%         end
-
-%         csvwriteh(csvf,data,fieldz);
-
+        
+        for di = 1:length(psFields)
+            cd = psFields{di};
+            
+            if isfield(jglData.postSurvey,cd)
+                comm = sprintf('%s{end+1} = jglData.postSurvey.%s',cd,cd);
+                eval(comm);
+            end
+        end
+    end
+    
+    for di = 1:length(demoFields)
+            cd = demoFields{di};
+        aname = sprintf('a%s',cd);
+        comm = sprintf('%s{ai} = %s;',aname,cd);
+        eval(comm);
+    end
+    for di = 1:length(psFields)
+            cd = psFields{di};
+        aname = sprintf('a%s',cd);
+        comm = sprintf('%s{ai} = %s;',aname,cd);
+        eval(comm);
     end
 end
 
-%%
+%% Age
+dirage = [aage{1}{:}];
+catage = [aage{2}{:}];
+longage = [aage{3}{:}];
+clage = [catage,longage];
+disp(sprintf('In the directions task the mean age was %2.0f +- %2.1f years.',mean(dirage),std(dirage)));
+disp(sprintf('In the category task the mean age was %2.0f +- %2.1f years.',mean(clage),std(clage)));
 
-for ni = 1:length(names)
-    disp(names{ni});
-end
+%% Gender
+dirgen = cellfun(@(x) strcmp(x,'male'),agender{1});
+catgen = cellfun(@(x) strcmp(x,'male'),agender{2});
+longen = cellfun(@(x) strcmp(x,'male'),agender{3});
+clgen = [catgen,longen];
+disp(sprintf('In the directions task there were %i male and %i female participants',sum(dirgen),length(dirgen)-sum(dirgen)));
+disp(sprintf('In the category task there were %i male and %i female participants',sum(clgen),length(clgen)-sum(clgen)));
 
-%%
-  fsigm = @(param,xval) param(1)+(param(2)-param(1))./(1+10.^((param(3)-xval)*param(4)))
+%% ruleDir
 
-X = [repmat(pf(1),45,1);repmat(pf(2),45,1);repmat(pf(3),45,1);repmat(pf(4),45,1)];
-est_params = sigm_fit(X,pcorrects{piG}(:));
-%% Testing weibull fit for dirs
+% re-coding
+recoded = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1];
+disp(sprintf('In the directions task %2.0f%% of participants reported the correct rule after 75 trials.',sum(recoded)/length(recoded)*100));
+%% ruleKnownDir
 
-pi=3.1415927;
-adiffs = round(360/2/pi*abs(jglData.rot2 - jglData.rot1)*1000)/1000;
-pf = unique(adiffs);
-piG = 3; % which pullfrom group
+%% ruleCat
 
-pc = mean(pcorrects{piG},1);
-pcste = sqrt(pc.*(1-pc)/size(pcorrects{piG},1));
+% re-coding
+recoded150 = [0,1,0,0,0,0,0,0,-1,0,-1,-1,0,0,-1,0,-1,-1,-1,0,-1,0,0,0,0,0,0,0,-1,0,0,0,1,-1,0,-1,0,1,0,0,0,-1,1,0,-1,0,0];
+recoded300 = [0,0,-1,-1,0,-1,-1,-1,0,0,-1,-1,0,-1,0,1];
 
-figure
-errorbar(pf,pc,pcste,'-g');
-hold on
-% plot(0:100,fsigm(est_params,0:100))
+disp(sprintf('Of the %i participants who did 150 trials %2.0f%% understood the rule, %2.0f%% were on the right track, and %2.0f%% had no idea or only knew that matching directions worked',length(recoded150),sum(recoded150==1)/length(recoded150)*100,sum(recoded150==-1)/length(recoded150)*100,sum(recoded150==0)/length(recoded150)*100));
+disp(sprintf('Of the %i participants who did 300 trials %2.0f%% understood the rule, %2.0f%% were on the right track, and %2.0f%% had no idea or only knew that matching directions worked',length(recoded300),sum(recoded300==1)/length(recoded300)*100,sum(recoded300==-1)/length(recoded300)*100,sum(recoded300==0)/length(recoded300)*100));
 
-pcs = pcorrects{piG};
-for si = 1:size(pcs,1)
-    p = plot(pf,pcs(si,:),'-k');
-%     alpha(p,.1)
-end
-% x = 0:90;
-% figure, hold on
-% plot(x,weibull(x,avgparams));
-plot([0,15,30,45,60,75],[0.05,.08,.22,.69,.9,.99],'-b');
-hold off
-fullleg = {'First 25 Trials','25-50 Trials','50-75 Trials','Informed Task','Monkeys'};
-legend(fullleg([piG 5]));
-% legend('Humans','Monkeys');
-axis([-5 80 -.05 1.05])
+%% Smoothness
+dirsmo = [asmoothness{1}{:}];
+catsmo = [asmoothness{2}{:}];
+longsmo = [asmoothness{3}{:}];
+clsmo = [catsmo,longsmo];
+disp(sprintf('%i/%i direction participants reported a non-smooth display',sum(dirsmo<5),length(dirsmo)));
+disp(sprintf('%i/%i category participants reported a non-smooth display',sum(clsmo<5),length(clsmo)));
+
+%% attention
+
+%% fixation
+dirfix = cellfun(@(x) strcmp(x,'yes'),afixation{1});
+catfix = cellfun(@(x) strcmp(x,'yes'),afixation{2});
+lonfix = cellfun(@(x) strcmp(x,'yes'),afixation{3});
+clfix = [catfix,lonfix];
+disp(sprintf('%i/%i direction participants reported fixating the cross (this was not explicitly asked for)',sum(dirfix),length(dirfix)));
+disp(sprintf('%i/%i category participants reported fixating the cross (this was not explicitly asked for)',sum(clfix),length(clfix)));
+
+%% motion
+dirmot = cellfun(@(x) strcmp(x,'yes'),amotion{1});
+catmot = cellfun(@(x) strcmp(x,'yes'),amotion{2});
+lonmot = cellfun(@(x) strcmp(x,'yes'),amotion{3});
+clmot = [catmot,lonmot];
+disp(sprintf('%i/%i direction participants reported eye jitter due to the motion stimulus',sum(dirmot),length(dirmot)));
+disp(sprintf('%i/%i category participants reported eye jitter due to the motion stimulus',sum(clmot),length(clmot)));
+
+%% before
+dirbef = cellfun(@(x) strcmp(x,'yes'),abefore{1});
+catbef = cellfun(@(x) strcmp(x,'yes'),abefore{2});
+lonbef = cellfun(@(x) strcmp(x,'yes'),abefore{3});
+clbef = [catbef,lonbef];
+disp(sprintf('%i/%i direction participants reported having done a similar experiment',sum(dirbef),length(dirbef)));
+disp(sprintf('%i/%i category participants reported having done a similar experiment',sum(clbef),length(clbef)));
+
+%% comments
